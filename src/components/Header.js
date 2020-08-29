e8c2575ac585814238ac004ad3e8f3dd3ed31782
GCaplan <gabriel.caplan@gmail.com>
2020-08-29 11:56:50 -0400

    boostrap template for header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Employee Directory</h1>
+      <p>
+        Click on carrots to filter by heading or use the search box to narrow
+        your results.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
